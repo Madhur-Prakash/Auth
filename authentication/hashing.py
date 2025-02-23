91ea1f4fde40574a1c9022b6d4e4e25ffc29dee6
Madhur <madhurprakash2005@gmail.com>
2025-02-24 02:14:54 +0530

    fixed bcrypt error

--- authentication/hashing.py
+++ authentication/hashing.py
@@ -1,10 +1,14 @@
-from passlib.context import CryptContext
+import bcrypt
 
-pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 class Hash():
     def bcrypt(password: str):
-        return pwd_cxt.hash(password)
+        pwd_bytes = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
+        return hashed_password
 
     async def verify(hashed_password,plain_password):
-        return pwd_cxt.verify(plain_password,hashed_password)
+        password_byte_enc = plain_password.encode('utf-8')
+        return bcrypt.checkpw(password = password_byte_enc , hashed_password = hashed_password)
