4510e1179ed92b4d2567dd77c3d24428758e0849
Madhur <madhurprakash2005@gmail.com>
2025-04-20 05:16:46 +0530

    updated pytest

--- test_api/test_login.py
+++ test_api/test_login.py
@@ -1,7 +1,6 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from app import app  # Use absolute import instead of relative import
 from authentication.helper.utils import get_country_name,generate_random_string
@@ -10,20 +9,20 @@ client = TestClient(app)
 
   
 
-# def test_patient_signup():
-#     response = client.post("/patient/signup", json={
-#         "email": "madhurprakash2005@gmail.com",
-#         "password": "123456",
-#         "first_name": "Madhur",
-#         "last_name": "Prakash",
-#         "phone_number": "1234567890",
-#         "country_code": "+91"})
-#     assert response.status_code == 201
-
-def test_patient_login():
-    response = client.post("/patient/login", json={
+def test_patient_signup():
+    response = client.post("/patient/signup", json={
         "email": "madhurprakash2005@gmail.com",
-        "password": "123456"
-    })
-    assert response.status_code == 200
+        "password": "123456",
+        "first_name": "Madhur",
+        "last_name": "Prakash",
+        "phone_number": "1234567890",
+        "country_code": "+91"})
+    assert response.status_code == 201
+
+# def test_patient_login():
+#     response = client.post("/patient/login", json={
+#         "email": "madhurprakash2005@gmail.com",
+#         "password": "123456"
+#     })
+#     assert response.status_code == 200
         
