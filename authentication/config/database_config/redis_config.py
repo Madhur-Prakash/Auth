9152618cc8f4d47935545e93addf9e11a42659bb
Madhur Prakash <madhurprakash2005@gmail.com>
2026-05-23 14:35:29 +0530

    Refactor code structure for improved readability and maintainability and migrated to redis

--- authentication/config/database_config/redis_config.py
+++ authentication/config/database_config/redis_config.py
@@ -1,5 +1,5 @@
 import os
-import aioredis
+import redis.asyncio as redis
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,11 +12,11 @@ try:
         REDIS_HOST = os.getenv("REDIS_HOST", "redis")
         REDIS_PORT = os.getenv("REDIS_PORT", "6379")
         redis_url = f"redis://{REDIS_HOST}:{REDIS_PORT}"
-        client = aioredis.from_url(redis_url, decode_responses=True)
+        client = redis.from_url(redis_url, decode_responses=True)
     else:
         REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
         REDIS_PORT = os.getenv("REDIS_PORT", "6379")
         redis_url = f"redis://{REDIS_HOST}:{REDIS_PORT}"
-        client = aioredis.from_url(redis_url, decode_responses=True)
+        client = redis.from_url(redis_url, decode_responses=True)
 except Exception as e:
     raise ConnectionError("Failed to connect to Redis")
